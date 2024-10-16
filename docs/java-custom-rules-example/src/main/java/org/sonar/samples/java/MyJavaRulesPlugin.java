9f63f28332224725dff433a1b2beb32dfd1ccb94
Stevan V <stevan.vanderwerf+1@sonarsource.com>
2024-10-16 12:34:40 -0500

    build plugin

--- docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/MyJavaRulesPlugin.java
+++ docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/MyJavaRulesPlugin.java
@@ -5,16 +5,29 @@
 package org.sonar.samples.java;
 
 import org.sonar.api.Plugin;
+import org.sonar.api.config.PropertyDefinition;
+import org.sonar.api.resources.Qualifiers;
 
 /**
  * Entry point of your plugin containing your custom rules
  */
 public class MyJavaRulesPlugin implements Plugin {
 
+  public static final String TEXT_STRINGS_KEY = "sonar.javacustom.text.strings";
+
   @Override
   public void define(Context context) {
     // server extensions -> objects are instantiated during server startup
-    context.addExtension(MyJavaRulesDefinition.class);
+    context.addExtensions(
+        PropertyDefinition.builder(TEXT_STRINGS_KEY)
+          .name("Text strings")
+          .description("Comma-separated list of text strings for files to analyze.")
+          .defaultValue(TEXT_STRINGS_KEY)
+          .multiValues(true)
+          .category("CustomPlugin")
+          .onQualifiers(Qualifiers.PROJECT)
+          .build(),
+    MyJavaRulesDefinition.class);
 
     // batch extensions -> objects are instantiated during code analysis
     context.addExtension(MyJavaFileCheckRegistrar.class);
